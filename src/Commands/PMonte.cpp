52829e1aed7228f3e04647cacc241227bdd6bed8
Ultime 5528 <keranna.first@gmail.com>
2016-02-10 17:30:32 -0500

    Pistons Roues Pivot

--- src/Commands/PMonte.cpp
+++ src/Commands/PMonte.cpp
@@ -24,7 +24,8 @@ PMonte::PMonte(): Command() {
 
 // Called just before this Command runs the first time
 void PMonte::Initialize() {
-
+	Robot::rouesPivot->PistonGaucheExtend();
+	Robot::rouesPivot->PistonDroitExtend();
 }
 
 // Called repeatedly when this Command is scheduled to run
@@ -34,16 +35,17 @@ void PMonte::Execute() {
 
 // Make this return true when this Command no longer needs to run execute()
 bool PMonte::IsFinished() {
-    return false;
+    return true;
 }
 
 // Called once after isFinished returns true
 void PMonte::End() {
-
+	Robot::rouesPivot->PistonGaucheOff();
+	Robot::rouesPivot->PistonDroitOff();
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
 void PMonte::Interrupted() {
-
+	End();
 }
--- src/Commands/PMonteDroite.cpp
+++ src/Commands/PMonteDroite.cpp
@@ -24,7 +24,7 @@ PMonteDroite::PMonteDroite(): Command() {
 
 // Called just before this Command runs the first time
 void PMonteDroite::Initialize() {
-
+	Robot::rouesPivot->PistonDroitExtend();
 }
 
 // Called repeatedly when this Command is scheduled to run
@@ -34,16 +34,16 @@ void PMonteDroite::Execute() {
 
 // Make this return true when this Command no longer needs to run execute()
 bool PMonteDroite::IsFinished() {
-    return false;
+    return true;
 }
 
 // Called once after isFinished returns true
 void PMonteDroite::End() {
-
+	Robot::rouesPivot->PistonDroitOff();
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
 void PMonteDroite::Interrupted() {
-
+	End();
 }
