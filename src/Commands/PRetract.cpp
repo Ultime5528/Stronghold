f4f0c29470607590d4c9344e8ff7364591a2a5a3
Ultime 5528 <keranna.first@gmail.com>
2016-02-10 17:31:36 -0500

    Pistons Roues Pivot essai #2

--- src/Commands/PRetract.cpp
+++ src/Commands/PRetract.cpp
@@ -24,7 +24,8 @@ PRetract::PRetract(): Command() {
 
 // Called just before this Command runs the first time
 void PRetract::Initialize() {
-
+	Robot::rouesPivot->PistonGaucheRetract();
+	Robot::rouesPivot->PistonDroitRetract();
 }
 
 // Called repeatedly when this Command is scheduled to run
@@ -34,16 +35,17 @@ void PRetract::Execute() {
 
 // Make this return true when this Command no longer needs to run execute()
 bool PRetract::IsFinished() {
-    return false;
+    return true;
 }
 
 // Called once after isFinished returns true
 void PRetract::End() {
-
+	Robot::rouesPivot->PistonGaucheOff();
+	Robot::rouesPivot->PistonDroitOff();
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
 void PRetract::Interrupted() {
-
+	End();
 }
